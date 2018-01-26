4a176eaa7fbe7ede3eea2bd93d17fb45df20f736
Paolo Marino <paolo.marino@spindox.it>
2018-01-26 14:41:27 +0100

    Ugly `ps` parses

--- src/os.rs
+++ src/os.rs
@@ -2,6 +2,16 @@ pub use nix::Result;
 
 use nix::sys::signal;
 use nix::unistd::{Pid, getpid};
+use std::process::Command;
+use std::str::FromStr;
+
+#[derive(Clone, Debug)]
+pub struct Process {
+  pid: i32,
+  tty: String,
+  time: String,
+  cmd: String,
+}
 
 pub fn kill(pid: i32) -> Result<()> {
   signal::kill(Pid::from_raw(pid), signal::SIGTERM)
@@ -9,4 +19,34 @@ pub fn kill(pid: i32) -> Result<()> {
 
 pub fn current_pid() -> i32 {
   i32::from(getpid())
+}
+
+pub fn ps(pid: i32) -> Option<Process> {
+  let mut ps = Command::new("ps");
+
+  ps.arg("-p").arg(format!("{}", pid).as_str());
+
+  match ps.output() {
+    Ok(out) => parse_ps_out(format!("{}", String::from_utf8_lossy(&out.stdout))),
+    Err(_) => None,
+  }
+}
+
+fn parse_ps_out(out: String) -> Option<Process> {
+  let mut lines: Vec<&str> = out.split("\n").collect();
+  
+  lines.remove(0);
+  lines.pop();
+  let procs: Vec<Process> = lines.iter().map(|p| { 
+    let fields: Vec<&str> = p.split(" ").collect();
+
+    Process {
+      pid: FromStr::from_str(fields[0]).unwrap(),
+      tty: format!("{}", fields[1]),
+      time: format!("{}", fields[2]),
+      cmd: format!("{}", fields[3]),
+    }
+  }).collect();
+
+  if procs.len() > 0 { Some(procs[0].clone()) } else { None } 
 }
